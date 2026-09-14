40a342ab639ba423a24759172989c5272049fd87
Avinash Upadhyay <avinashupa985@gmail.com>
2026-09-14 18:57:07 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -79,7 +79,12 @@ module.exports.login = async (req, res) => {
         }
 
 
-        return res.status(200).cookie("token", token,{maxAge:1*24*60*60*1000,httpOnly:true, sameSite:'strict'}).json({
+      return res.status(200).cookie("token", token, {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+}).json({
             message:`Welcome Back ${user.fullname}`,
             user,
             success:true,
