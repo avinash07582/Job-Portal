cf68d1879ffb22e13bd26563373b6e00c30b64f9
Avinash Upadhyay <avinashupa985@gmail.com>
2026-09-14 23:19:35 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,7 +1,41 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+
+// const isAuthenticated = async (req, res, next) => {
+//     try {
+//         const token = req.cookies.token;
+
+//         if (!token) {
+//             return res.status(401).json({
+//                 error: "You are not authenticated"
+//             });
+//         }
+
+//         const decode = jwt.verify(
+//             token,
+//             process.env.JWT_SECRET
+//         );
+
+//         req.id = decode.userId;
+
+//         next();
+
+//     } catch (error) {
+//         console.log("Auth Error:", error);
+
+//         return res.status(401).json({
+//             error: "Invalid or expired token"
+//         });
+//     }
+// };
+
+// module.exports = isAuthenticated;
 
 const isAuthenticated = async (req, res, next) => {
     try {
+
+        console.log("COOKIES:", req.cookies);
+        console.log("TOKEN:", req.cookies.token);
+
         const token = req.cookies.token;
 
         if (!token) {
@@ -10,10 +44,7 @@ const isAuthenticated = async (req, res, next) => {
             });
         }
 
-        const decode = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
 
         req.id = decode.userId;
 
@@ -27,5 +58,3 @@ const isAuthenticated = async (req, res, next) => {
         });
     }
 };
-
-module.exports = isAuthenticated;
